a3e15b58e8f031869fdab1738fb39fe7665a5132
kenya6111 <kenyanke6111@gmail.com>
2025-10-13 00:18:26 +0900

    array

---
+++ hamusan-typescript/src/array.ts
@@ -0,0 +1,22 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3, 4, 5];
+
+let strings: Array<string> = ['aaa', 'bbb', 'ccc'];
+
+let nijigenhairetsu: Array<Array<string>> = [
+  ['aaa', 'bbb', 'ccc'],
+  ['aaa', 'bbb', 'ccc'],
+  ['aaa', 'bbb', 'ccc'],
+];
+let nijigenhairetsu2: string[][] = [
+  ['aaa', 'bbb', 'ccc'],
+  ['aaa', 'bbb', 'ccc'],
+  ['aaa', 'bbb', 'ccc'],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'japan'];
+
+console.log(nijigenhairetsu);
