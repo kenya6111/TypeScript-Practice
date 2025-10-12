4a3a1911403077b1b07cb661f28d33c5be116e9e
kenya6111 <kenyanke6111@gmail.com>
2025-10-13 01:23:33 +0900

    null-undefined

---
+++ hamusan-typescript/src/null-undefined.ts
@@ -0,0 +1,7 @@
+export {};
+
+let absence: null = null;
+// absence = 'hello'; //Type '"hello"' is not assignable to type 'null'.ts(2322)
+
+let data: undefined = undefined;
+// data = 111; //Type '111' is not assignable to type 'undefined'.ts(2322)
