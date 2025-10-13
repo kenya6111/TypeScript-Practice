7d357a85685f44c40aec153ab538a9fe73408d40
kenya6111 <kenyanke6111@gmail.com>
2025-10-13 22:47:36 +0900

    nullあｂぇ

---
+++ hamusan-typescript/src/nullable-types.ts
@@ -0,0 +1,7 @@
+export {};
+
+// let profile: { name: string; age: number } = {
+//   name: 'ham',
+//   // age: null, //Type 'null' is not assignable to type 'number'.ts(2322)
+//   // tsconfig.tsのstrictNullChecksをfalseにするとエラーは消える
+// };
