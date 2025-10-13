10d62c30c1ce02d54867e51608c205b77b091d7d
kenya6111 <kenyanke6111@gmail.com>
2025-10-13 14:40:12 +0900

    arrow

---
+++ hamusan-typescript/src/arrow.ts
@@ -0,0 +1,9 @@
+export {};
+
+let bmi = (height: number, weight: number): number => {
+  return weight / (height * height);
+};
+let bmi2 = (height: number, weight: number): number =>
+  weight / (height * height);
+
+console.log(bmi(1.78, 86));
