1a1e860781f7449b751f411bb6e3b3da57603b51
kenya6111 <kenyanke6111@gmail.com>
2025-10-13 21:22:35 +0900

    generics

--- hamusan-typescript/src/generics.ts
+++ hamusan-typescript/src/generics.ts
@@ -39,3 +39,7 @@ class Mirror2<T> {
 
 console.log(new Mirror2<string>('hello').echo());
 console.log(new Mirror2<number>(123).echo());
+
+// **ジェネリクス（generics）**は英語の generic = 「一般的な／特定しない」 から来ています。
+// 語源は ラテン語 genus（種・種類）／ギリシャ語 genos（種族・類）で、「ある“種類”に属するけど、個別には特定しない」という意味合い。
+// だから TypeScript の T みたいな型パラメータは、「まだ具体的な型を特定せず、“種類（family of types）”を一般化して扱う」ための記号、というわけです。
